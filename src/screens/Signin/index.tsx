54a616f07e5d2d9cc66d09f1ba81c29924225d56
Kayre-Scott-Primon <kayrescott@gmail.com>
2021-06-22 22:42:35 -0300

    Primeira aula

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { 
+  View, 
+  Text, 
+  TextInput,
+  Image,
+  StatusBar,
+} from "react-native";
+import { styles } from './style'
+import IllustrationImg from '../../assets/illustration.png'
+import { ButtonIcon } from "../../components/ButtonIcon";
+
+export function Signin(){
+
+  return(
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent/>
+      <Image source={IllustrationImg} style={styles.image} resizeMode='stretch'/>
+      <View style={styles.content}>
+        <Text style={styles.title}>Organize{`\n`}suas jogatinas{`\n`}facilmente</Text>
+        <Text style={styles.subtitle}>Crie grupos para jogar seus games{`\n`}favoritos com seus amigos</Text>
+        <ButtonIcon activeOpacity={.5} title='Entrar com Discord'/>
+      </View>
+    </View>
+  )
+}
